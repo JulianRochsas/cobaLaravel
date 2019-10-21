d0dc2d8774efe3190d27a966a1f5b96a0a161cb8
JulianRochsas <fainanda.lead@gmail.com>
2019-10-21 21:59:22 +0700

    mengganti template front end dengan template argon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layout/main')
 
-@section('title', 'Web Programming Polinela')
+@section('title', 'Sistem Pengolahan Data Mahasiswa')
 
 @section('container')
     <div class="container">
         <div class="row">
             <div class="col-10">
-                <h1 class="mt-3">Hello, world!</h1>
+                <h1 class="mt-3">Selamat Datang!</h1>
             </div>
         </div>
     </div>
